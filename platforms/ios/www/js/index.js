2bc4e894e3a2972d9d8c3c9933f625dcff2f7ca0
I Dewa Gede Anthasenna <antax666@gmail.com>
2018-08-17 17:33:24 +0800

    Update App V2

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -16,42 +16,81 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var dbSize = 5 * 1024 * 1024; // 5MB
+
+var db = openDatabase("Todo", "", "Todo manager", dbSize, function() {
+    console.log('db successfully opened or created');
+});
+
+function onSuccess(transaction, resultSet) {
+    console.log('Query completed: ' + JSON.stringify(resultSet));
+}
+
+function onError(transaction, error) {
+    console.log('Query failed: ' + error.message);
+}
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
+
 var app = {
     // Application Constructor
     initialize: function() {
         
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-
-        var dbSize = 5 * 1024 * 1024; // 5MB
-
-        var db = openDatabase("Todo", "", "Todo manager", dbSize, function() {
-            console.log('db successfully opened or created');
-        });
-
+        
+        //CREATE TABLE
         db.transaction(function (tx) {
             tx.executeSql("CREATE TABLE IF NOT EXISTS todo(ID INTEGER PRIMARY KEY ASC, todo TEXT, added_on TEXT)",
                 [], onSuccess, onError);
-            tx.executeSql("INSERT INTO todo(todo, added_on) VALUES (?,?)", ['super my todo item', new Date().toUTCString()], onSuccess, onError);
         });
 
-        function onSuccess(transaction, resultSet) {
-            console.log('Query completed: ' + JSON.stringify(resultSet));
-        }
+        $('#data').DataTable();
 
-        function onError(transaction, error) {
-            console.log('Query failed: ' + error.message);
-        }
+        includeHTML();
+    },
+
+    doQuery : function(){
+        //INSERT
+        db.transaction(function (tx) {
+            tx.executeSql("INSERT INTO todo(todo, added_on) VALUES (?,?)", ['super my todo item', new Date().toUTCString()], onSuccess, onError);
+        });
 
         //SELECT
         db.transaction(function (tx) { 
         tx.executeSql('SELECT * FROM todo', [], function (tx, results) { 
               var len = results.rows.length, i; 
               msg = "<p>Found rows: " + len + "</p>"; 
-              document.querySelector('#status').innerHTML +=  msg; 
+              //document.querySelector('#status').innerHTML +=  msg; 
 
               for(var i=0; i<results.rows.length; i++) {
                 var row = results.rows.item(i);
-                document.querySelector('#status').innerHTML += row["todo"]+"<br/>";
+                //document.querySelector('#status').innerHTML += row["todo"]+"<br/>";
               }
           
            }, null); 
